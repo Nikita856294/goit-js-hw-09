1b912ce54c4308eafc053b302f7d3629cf8f73e7
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-11-08 02:03:09 +0200

    сделал 1 задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
+let isActive = false;
+
+buttonStart.addEventListener('click', changeBackgroundColor);
+buttonStop.addEventListener('click', stopChangeBackgroundColor);
+
+function changeBackgroundColor() {
+  if (isActive === false) {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+      console.log(timerId);
+    }, 1000);
+    isActive = true;
+  }
+}
+
+function stopChangeBackgroundColor() {
+  if (isActive === true) {
+    clearInterval(timerId);
+    isActive = false;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
